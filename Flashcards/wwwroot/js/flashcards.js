6095f202990f23002129f58152beefa64640b8df
Iair Kolberg <iair.kolberg@tipalti.com>
2024-07-04 23:03:57 +0300

    fixed small bugs
    fixed style in categories page

--- Flashcards/wwwroot/js/flashcards.js
+++ Flashcards/wwwroot/js/flashcards.js
@@ -1,5 +1,6 @@
 let currentFlashcardIndex = 0;
 let flashcards = [];
+let showingQuestion = true;
 
 window.onload = function () {
     let flashcardsData = document.getElementById("flashcards-data");
@@ -9,6 +10,14 @@ window.onload = function () {
     } else {
         console.error("flashcards-data element not found");
     }
+
+    // Add event listeners to the table rows
+    document.querySelectorAll('.flashcard-row').forEach(row => {
+        row.addEventListener('click', function () {
+            currentFlashcardIndex = parseInt(this.dataset.index);
+            showFlashcard();
+        });
+    });
 };
 
 window.startGame = function () {
@@ -37,6 +46,7 @@ window.startGameFromId = function () {
 };
 
 window.showFlashcard = function () {
+    showingQuestion = true;
     let flashcard = flashcards[currentFlashcardIndex];
     console.log("Showing flashcard:", flashcard); // Debugging line
     if (flashcard) {
@@ -51,7 +61,7 @@ window.showFlashcard = function () {
         });
 
         if (flashcardContent && flashcardContentBack && flashcardModalLabel) {
-            flashcardModalLabel.textContent = "Flashcard ID: " + flashcard.Id;
+            flashcardModalLabel.textContent = flashcard.Category + " " + flashcard.Id;
             flashcardContent.innerHTML = flashcard.Question.replace(/\n/g, "<br>");
             flashcardContentBack.innerHTML = flashcard.Answer ? flashcard.Answer.replace(/\n/g, "<br>") : "No answer available.";
             $('#flashcardModal').modal('show');
@@ -78,4 +88,10 @@ window.previousFlashcard = function () {
 window.flipCard = function (event) {
     let card = event.currentTarget.closest('.flip-container');
     card.classList.toggle("flipped");
+    if (showingQuestion) {
+        document.getElementById('watermark').innerText = "Answer";
+    } else {
+        document.getElementById('watermark').innerText = "Question";
+    }
+    showingQuestion = !showingQuestion;
 };
